4018effd91c3be8bda2fef27d042bbb5454f75a6
Xiao <mail52@26.com>
2020-09-03 10:40:34 +0800

    test

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,7 +9,7 @@ class CategoryController extends Controller
 {
     public function index($category_id = null) {
         if(!$category_id) {
-            return ArticleCategory::collection(Category::withCount('articles')->get());
+            return ArticleCategory::collection(Category::top()->withCount('articles')->get());
         }
         return ArticleCategory::collection(Category::where('parent_id', $category_id)->withCount('articles')->get());
     }
