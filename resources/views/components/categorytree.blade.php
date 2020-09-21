61bbd8ff5472e90ed8dd7cf6ae289788ab274779
Xiao <mail52@26.com>
2020-09-21 09:38:31 +0800

    ci

--- resources/views/components/categorytree.blade.php
+++ resources/views/components/categorytree.blade.php
@@ -4,11 +4,11 @@
     @if(!empty($options))
         
         @foreach($options as $k =>  $v)
-            @if($v->subcates->isEmpty())
+            @if($v->subcates()->doesntExist())
               <option {{ isset($selected) && $v->id == $selected?'selected': '' }} value="{{$v->id}}">{{$v->name}}</option>
             @else
 
-            
+            <!-- <option value="{{$v->id}}">{{$v->name}}</option> -->
             @foreach($v->subcates as $k1 => $v1)
                 <option value="{{$v1->id}}" {{ isset($selected) && $v1->id == $selected?'selected' : '' }}>--{{$v1->name}}</option>
             @endforeach
